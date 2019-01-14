f9d877321610935933492c8e552b8ae47b6c99ce
Kyle Goodwin <kjgoodwins@gmail.com>
2019-01-13 23:24:29 -0800

    All tests passing

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,10 +10,61 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        
+        switch args[args.count - 1] {
+        case "count":
+            return args.count-1;
+        case "avg":
+            var total = 0
+            var element = 0
+            while(args[element] != "avg"){
+                total = total + Int(args[element])!
+                element = element + 1
+            }
+            if( element == 0){
+                return 0
+            }
+            return ( total/element)
+        case "fact":
+            var total = 1;
+            var count = 1;
+            
+            if( args[0] == "fact"){
+                return 0
+            }
+            
+            while( count <= Int(args[0])!){
+                total = total * count
+                count = count + 1
+            }
+        
+            return total
+            
+        default:
+            
+            switch args[1] {
+            case "+":
+                return Int(args[0])! + Int(args[2])!
+            case "-":
+                return Int(args[0])! - Int(args[2])!
+            case "/":
+                return Int(args[0])! / Int(args[2])!
+            case "%":
+                return Int(args[0])! % Int(args[2])!
+            case "*":
+                return Int(args[0])! * Int(args[2])!
+                
+            default:
+                return 0
+                
+            }
+        }
+        
     }
     
     public func calculate(_ arg: String) -> Int {
         return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
+        
     }
 }
 
